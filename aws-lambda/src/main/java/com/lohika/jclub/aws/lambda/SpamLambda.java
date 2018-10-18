e4ad5683ad332e98f012c19f99daa7dda2dc3fcf
RomanKniazevych <aboruhen@gmail.com>
2018-10-18 10:36:41 +0300

    Create SNS topic; Send email via SNS

--- aws-lambda/src/main/java/com/lohika/jclub/aws/lambda/SpamLambda.java
+++ aws-lambda/src/main/java/com/lohika/jclub/aws/lambda/SpamLambda.java
@@ -6,19 +6,16 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClient;
-import com.amazonaws.services.sns.model.MessageAttributeValue;
 import com.amazonaws.services.sns.model.PublishRequest;
 import com.amazonaws.services.sns.model.PublishResult;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.amazonaws.services.sns.model.SubscribeRequest;
 
 public class SpamLambda {
     //    SnsClient snsClient = SnsClient.builder()
 //            .region(Region.EU_WEST_1)
 //            .build();
     AmazonSNS snsClient = AmazonSNSClient.builder()
-            .withRegion(Regions.EU_WEST_1)
+            .withRegion(Regions.EU_CENTRAL_1)
             .build();
 
 
@@ -30,38 +27,23 @@ public class SpamLambda {
         event.getRecords().stream()
                 .filter(r -> r.getEventName().equals("MODIFY"))
                 .forEach(r -> {
-//                            PublishRequest request = PublishRequest.builder()
-//                                    .message("Zalyshok: " + r.getDynamodb().getNewImage().get("deposit").getN())
-////                                    .phoneNumber("+380631773525")
-////                                    .topicArn("arn:aws:sns:eu-west-1:713791747198:java_club_topic_sms")
-//                                    .targetArn("arn:aws:sns:eu-west-1:713791747198:java_club_topic_sms:0abf1fcb-e708-42e0-8f98-150002ace035")
-//                                    .build();
-                            logger.log("************************************************************************");
-                            Map<String, MessageAttributeValue> smsAttributes = new HashMap<>();
-                            smsAttributes.put("AWS.SNS.SMS.SenderID", new MessageAttributeValue()
-                                    .withStringValue("mySenderID") //The sender ID shown on the device.
-                                    .withDataType("String"));
-                            smsAttributes.put("AWS.SNS.SMS.SMSType", new MessageAttributeValue()
-                                    .withStringValue("Promotional") //Sets the type to promotional.
-                                    .withDataType("String"));
-
-                            PublishResult publishResponse = snsClient.publish(new PublishRequest()
-                                    .withMessage("Zalyshok: " + r.getDynamodb().getNewImage().get("deposit").getN())
-//                                    .withPhoneNumber("+380631773525")
-                                    .withTopicArn("arn:aws:sns:eu-west-1:713791747198:java_club_topic_sms")
-                                    .withMessageAttributes(smsAttributes)
+                            //Create Topic
+                            String topicArn = "arn:aws:sns:eu-central-1:713791747198:java_club_topic";
+                            SubscribeRequest subscribeRequest = new SubscribeRequest(
+                                    topicArn,
+                                    "email",
+                                    "customEmail@example.com"
                             );
-                            //PublishResponse publishResponse = snsClient.publish(request);
+                            snsClient.subscribe(subscribeRequest);
 
-                            logger.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-//                            logger.log("Message ID: " + publishResponse.messageId());
-//                            logger.log("Response: " + publishResponse.toString());
-                            logger.log("Message ID: " + publishResponse.getMessageId());
-                            logger.log("Response Code: " + publishResponse.getSdkHttpMetadata().getHttpStatusCode());
-                            logger.log("Response Headers: " + publishResponse.getSdkHttpMetadata().getHttpHeaders());
-                            logger.log("Response: " + publishResponse.toString());
-
+                            //Send email
+                            PublishRequest publishRequest = new PublishRequest();
+                            publishRequest.setMessage("Email test message");
+                            publishRequest.setTopicArn(topicArn);
+                            publishRequest.setSubject("Test Subject");
+                            PublishResult publish = snsClient.publish(publishRequest);
                         }
                 );
     }
+
 }
